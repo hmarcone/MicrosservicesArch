8d6234889351d5b0239b7741a6410e21c175fad6
Humberto Marcone <hmarcone@gmail.com>
2021-07-09 18:28:24 -0300

    Add project files.

---
+++ Catalogo.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,71 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace Catalog.API.Data
+{
+    public static class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "60e45c01fc067bbf9376cb2b",
+                    Name = "IPhone X",
+                    Category = "Smart Phone",
+                    Description = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
+                    Image = "product-1.png",
+                    Price = 950.00m
+                },
+                new Product()
+                {
+                    Id = "60e45c2251a601e4f2ea7a1e",
+                    Name = "Samsung 10",
+                    Category = "Smart Phone",
+                    Description = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
+                    Image = "product-2.png",
+                    Price = 850.00m
+                },
+                new Product()
+                {
+                    Id = "60e45c62aecc2b6f5ba9f791",
+                    Name = "Xiaomi Mi 9",
+                    Category = "White Appliances",
+                    Description = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
+                    Image = "product-3.png",
+                    Price = 450.00m
+                },
+                new Product()
+                {
+                    Id = "60e45cb6cd14cb94efa09183",
+                    Name = "HTC U11+ Plus",
+                    Category = "Smart Phone",
+                    Description = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
+                    Image = "product-4.png",
+                    Price = 380.00m
+                },
+                new Product()
+                {
+                    Id = "60e45d29b92333eb401ce7d4",
+                    Name = "LG G7 ThinQ",
+                    Category = "Home Kitchen",
+                    Description = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
+                    Image = "product-5.png",
+                    Price = 280.00m
+                }
+            };
+        }
+    }
+}
